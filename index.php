5c16d460907e9e7ec4bf32e1c294278d820684ac
QuentinPerret <quentinprrt33@gmail.com>
2022-04-25 19:16:03 +0200

    prepare merge

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+
+<!doctype html>
+<html>
+
+<?php require_once "Include/head.php"; ?>
+
+<body>
+    <div class="container">
+        <?php require_once "Include/header.php"; ?>
+
+</br>
+</br>
+</br>
+</br>
+
+        <?php require_once "Include/footer.php"; ?>
+    </div>
+
+    <?php require_once "Include/scripts.php"; ?>
+</body>
+
+</html>
