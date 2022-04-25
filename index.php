e3ca263b7bdbcd255b1c797800aa301a21a93fb4
QuentinPerret <quentinprrt33@gmail.com>
2022-04-25 19:41:45 +0200

    Add test connected for navbar

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php
 session_start();
-?>
+$_SESSION['login'] = "my login"; ?>
 
 <!doctype html>
 <html>
 
-<?php require_once "Include/head.php"; ?>
+<?php require_once "includes/head.php"; ?>
 
 <body>
     <div class="container">
-        <?php require_once "Include/header.php"; ?>
+        <?php require_once "includes/header.php"; ?>
 
 </br>
 </br>
 </br>
 </br>
 
-        <?php require_once "Include/footer.php"; ?>
+        <?php require_once "includes/footer.php"; ?>
     </div>
 
-    <?php require_once "Include/scripts.php"; ?>
+    <?php require_once "includes/scripts.php"; ?>
 </body>
 
 </html>
