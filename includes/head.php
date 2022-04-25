bc929d553928db68dc697644c83b31f9ed78ef2f
QuentinPerret <quentinprrt33@gmail.com>
2022-04-25 19:20:21 +0200

    resolve merge errors

--- includes/head.php
+++ includes/head.php
@@ -3,6 +3,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <title>MyMovies <?php if (isset($pageTitle)) echo "- $pageTitle"; ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>JeuHero </title>
 </head>
