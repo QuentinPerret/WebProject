f5b5efd4310ea051d6af06320280d267a099099c
Antoine005 <94908752+Antoine005@users.noreply.github.com>
2022-04-26 11:26:09 +0200

    modif

--- signup.php
+++ signup.php
@@ -5,11 +5,11 @@ session_start();
 <!doctype html>
 <html>
 
-<?php require_once "Include/head.php"; ?>
+<?php require_once "includes/head.php"; ?>
 
 <body>
     <div class="container-fluid">
-        <?php require_once "Include/headernoco.php"; ?>
+        <?php require_once "includes/headernoco.php"; ?>
     
 </br>
 </br>
@@ -58,10 +58,10 @@ session_start();
         
     </div>
 
-        <?php require_once "Include/footer.php"; ?>
+        <?php require_once "includes/footer.php"; ?>
     </div>
 
-    <?php require_once "Include/scripts.php"; ?>
+    <?php require_once "includes/scripts.php"; ?>
 </body>
 
 </html>
