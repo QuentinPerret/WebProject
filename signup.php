0a3045c99b3635638917fde22ed51db0df7ee002
Antoine005 <94908752+Antoine005@users.noreply.github.com>
2022-05-02 15:47:42 +0200

    resolve conflits

--- signup.php
+++ signup.php
@@ -4,8 +4,13 @@ session_start();
 
 <!doctype html>
 <html>
-
-<?php require_once "includes/head.php"; ?>
+    <?php 
+    require_once "includes/functions.php";
+    if(checkUser()){ 
+        addNewUser(); 
+        redirect("index.php");
+    }
+    require_once "includes/head.php";  ?>
 
 <body>
     <div class="container-fluid">
@@ -15,33 +20,52 @@ session_start();
     <div class="container" style="margin-top: 100px">
         <form>
             <div class="form-group row">
+        </br>
+        </br>
+        </br>
+        </br>
+        </br>
+        </br>
+    </div>
+        <form method="post" class="row g-2" action="signup.php" novalidate >
+        <div class="form-group row" >
+                <div class="col-sm-8">
+                    <div class="col-sm-6">
+                    </div>
+                    <label for="login"class="col-sm-2 col-form-label">Login</label>
+                    <div class="col-sm-4">
+                        <input type="text" class="form-control" name="login" id="login" placeholder="Login" required>
+                    </div>
+                </div>      
+            </div>
+            <div class="form-group row" >
                 <div class="col-sm-8">
                     <div class="col-sm-6">
                     </div>
                     <label for="email"class="col-sm-2 col-form-label">Email address</label>
                     <div class="col-sm-4">
-                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                        <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
                     </div>
                 </div>      
             </div>
             
-            <div class="form-group row">
+            <div class="form-group row" >
                 <div class="col-sm-8">
                     <div class="col-sm-6">
                     </div>
                     <label for="password" class="col-sm-2 col-form-label">Password</label>
                     <div class="col-sm-4">
-                        <input type="password" class="form-control" id="Password1" placeholder="Password">
+                        <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
                     </div>
                 </div>  
             </div>
-            <div class="form-group row">
+            <div class="form-group row" >
                 <div class="col-sm-8">
                     <div class="col-sm-6">
                     </div>
-                    <label for="password" class="col-sm-2 col-form-label">Confirm Password</label>
+                    <label for="password_conf" class="col-sm-2 col-form-label">Confirm Password</label>
                     <div class="col-sm-4">
-                        <input type="password" class="form-control" id="Password2" placeholder="Password">
+                        <input type="password" class="form-control" name="password_conf" id="password_conf" placeholder="Password" required>
                     </div>
                 </div>  
             </div>
@@ -50,10 +74,11 @@ session_start();
             <div class="form-group row" style="width:100px;" >
                 <button type="submit" class="btn btn-danger">Submit</button>
             </div>
+        <script src = "js/signup.js"></script>
         </form>
     </div>
     </div>
-
+    <div>
         <?php require_once "includes/footer.php"; ?>
     </div>
 
