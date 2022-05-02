33299988b25a8d77b63598f1c0041dfc89ed987d
Antoine005 <94908752+Antoine005@users.noreply.github.com>
2022-05-02 15:40:35 +0200

    story creaation0202

--- storyCreation.php
+++ storyCreation.php
@@ -62,13 +62,27 @@ $_SESSION['login'] = null; ?>
                                 <th scope="row">1</th>
                                 <td>Chapter</td>
                                 <td>Dragon</td>
+                                <td><a role="button" action="#">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+                                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+                                        </svg>
+                                    </a>
+                                </td>
+                                
                                 </tr>
                         <?php $tab=null;
                         foreach($tab as $key=>$ligne){?>
                             <tr>        
                                 <th scope="row"><?php echo($ligne)?></th>
                                 <td><?php echo($ligne["ch_title"])?></td>
-                                <td>Thornton</td>
+                                <td><a role="button" action="#">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+                                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+                                        </svg>
+                                    </a>
+                                </td>
                             </tr>
 
                         <?php } ?>
