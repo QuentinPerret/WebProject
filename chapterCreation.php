474c70a14e314ae9f8b0204b1c14132629b88525
Antoine Porte <Antoine.Porte@bordeaux-inp.fr>
2022-05-08 20:38:40 +0200

    0805

--- chapterCreation.php
+++ chapterCreation.php
@@ -80,7 +80,7 @@ $_SESSION['login'] = null; ?>
                 </div>
                 <div class="card mb-5">
                 <div class="card-body">
-                    <h4 class="card-title">Card title</h4>
+                    <h4 class="card-title">Summary</h4>
                     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="card-link">Card link</a>
