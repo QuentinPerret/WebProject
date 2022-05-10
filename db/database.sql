e9e8f6d603d0aa32c0fbd55eddbdbceed3acb43d
Antoine005 <94908752+Antoine005@users.noreply.github.com>
2022-05-10 16:06:39 +0200

    remove functions in nonwhere

--- db/database.sql
+++ db/database.sql
@@ -1,4 +1,3 @@
 create database if not exists mystory character set utf8 collate utf8_unicode_ci;
 use mystory;
-flush privileges;
 grant all privileges on mystory.* to 'mystory_user'@'localhost' identified by 'secret';
