a1f24a62d6e61c5a0ddb3c0d654a37fc00e465a5
Antoine Porte <Antoine.Porte@bordeaux-inp.fr>
2022-05-11 20:03:45 +0200

    templat stories V1

--- stories.php
+++ stories.php
@@ -22,6 +22,29 @@ session_start();?>
             <span class="page"></span>
             <span class="cover turn"></span>
         </div>
+        <div class="card mb-3" style="margin-right: 50px;">
+                <h3 class="card-header">Titre de l'histoire</h3>
+                <div class="card-body">
+                    <h5 class="card-title">The current chapter</h5>
+                    <h6 class="card-subtitle text-muted">Support card subtitle</h6>
+                </div>
+                
+                <div class="card-body">
+                    <p class="card-text">The story</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Cras justo odio</li>
+                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                    <li class="list-group-item">Vestibulum at eros</li>
+                </ul>
+                <div class="card-body">
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+                <div class="card-footer text-muted">
+                    2 days ago
+                </div>
+                </div>
 </div>
 
         <?php require_once "includes/footer.php"; ?>
