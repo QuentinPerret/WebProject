ff2f259cae6bb53caea273495f75b7c010e941b7
QuentinPerret <quentinprrt33@gmail.com>
2022-05-11 22:46:09 +0200

    correction little bug for link

--- includes/delLink.php
+++ includes/delLink.php
@@ -3,6 +3,9 @@ session_start();
 require_once 'functions.php';
 $idLink = $_GET['idLink'];
 $link = getLink(($idLink));
+$chId = $link['link_ch'];
+editCh($chId);
+editAllLink($chId);
 delLink($idLink);
-redirect('../chapterCreation.php?idCh='.$link['link_ch']);
+redirect('../chapterCreation.php?idCh='.$chId);
 ?>
