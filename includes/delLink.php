d8a9f29e5c874c287010ccc9de9c031d5bd6f58b
QuentinPerret <quentinprrt33@gmail.com>
2022-05-12 16:32:19 +0200

    corect error in delLink

--- includes/delLink.php
+++ includes/delLink.php
@@ -2,7 +2,8 @@
 session_start();
 require_once 'functions.php';
 $idLink = $_GET['idLink'];
-$link = getLink(($idLink));
+$link = getLink($idLink);
+editCh($link['link_ch']);
 delLink($idLink);
 redirect('../chapterCreation.php?idCh='.$link['link_ch']);
 ?>
